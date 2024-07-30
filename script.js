50a878989f8df34332e739649a707a5012ab7b52
Tiago <tiagopassos950@gmail.com>
2024-07-29 21:54:30 -0300

    Atualização

--- script.js
+++ script.js
@@ -24,3 +24,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const logoText = 'ProInvest';
     typeEffect(logoElement, logoText, 200);
 });
+
+// Contador de Promoção
+function startCountdown(duration, display) {
+    let timer = duration, days, hours, minutes, seconds;
+    setInterval(function () {
+        days = Math.floor(timer / (60 * 60 * 24));
+        hours = Math.floor((timer % (60 * 60 * 24)) / (60 * 60));
+        minutes = Math.floor((timer % (60 * 60)) / 60);
+        seconds = Math.floor(timer % 60);
+
+        display.textContent = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    let promoTime = 60 * 60 * 24 * 7; // 7 dias em segundos
+    let display = document.querySelector('#countdown');
+    startCountdown(promoTime, display);
+};
